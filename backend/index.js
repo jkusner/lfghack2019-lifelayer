16ab4a9b3565d0d84b3efe96785083dbfab35d26
Srini Laliwala <you@example.com>
2019-03-23 15:28:39 -0400

    post request for foreign api

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,37 @@
-lmao
+
+// call the packages we need
+var express = require('express');        // call express
+var app = express();                 // define our app using express
+var bodyParser = require('body-parser');
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;        // set our port
+
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
+
+var attrquery = {
+    attr_name: "",
+    comparison: "",
+    compare_to: "",
+}
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.post('/', function(req, res) {
+    res.json({ 'required': [attrquery, attrquery], 'optional': [attrquery]});
+});
+
+// more routes for our API will happen here
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/lifelayer', router);
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
