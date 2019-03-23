179ade1b1b61bbfa5609bc42a93a600bf60bf36f
Srini Laliwala <you@example.com>
2019-03-23 17:15:31 -0400

    endpoints added

--- backend/index.js
+++ backend/index.js
@@ -15,15 +15,53 @@ var port = process.env.PORT || 8080;        // set our port
 // =============================================================================
 var router = express.Router();              // get an instance of the express Router
 
+function rand_gen_key() {
+    var dict = 'abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIGKLMNOPQRSTUVWXYZ'
+    var randStr = ''
+    var len = dict.length-1
+    var i;
+    for( i = 0; i <= 128; i++) {
+        var n = Math.floor(Math.random() * len) + 0
+        randStr += dict.charAt(n)
+    }
+    console.log(randStr)
+    return randStr
+}
+
+function is_key_valid() {
+    while(status) {
+        
+    }
+}
+
+const ongoingReqs;
 var attrquery = {
     attr_name: "",
     comparison: "",
     compare_to: "",
+    app_name: "",
 }
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.post('/', function(req, res) {
-    res.json({ 'required': [attrquery, attrquery], 'optional': [attrquery]});
+router.post('/request', function(req, res) {
+    var key = rand_gen_key();
+    
+    ongoingReqs[key] = {
+        'status': 'waiting'
+    }
+    res.json({ 'key': key, 'status': ongoingReqs[key], 'required': [attrquery, attrquery], 'optional': [attrquery]});
 });
+
+router.get('/status/:key', function(req, res) {
+    var key = req.param.key;
+    res.json('status': ongoingReqs[key])
+});
+
+router.get('/status/:key', function(req, res) {
+    var key = req.param.key;
+    res.json({'status': ongoingReqs[key].});
+});
+
+
 
 // more routes for our API will happen here
 
